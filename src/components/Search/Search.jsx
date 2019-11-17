c7205f2a1f1d76e8d9ecff169a13f4b7c3179198
Mike Wong <wongmdesign@gmail.com>
2019-11-16 22:37:42 -0500

    corrected to flexbox

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -14,7 +14,7 @@ export default class Search extends Component {
     e.preventDefault()
   }
 
-  onChange = e => {
+  onHandleChange = e => {
     if (e.target.name === 'forked') {
       this.setState({
         forked: !this.state.checked
@@ -31,35 +31,38 @@ export default class Search extends Component {
     return (
       <div id='fields'>
         <form onSubmit={this.onFormSubmit}>
-          <div id='repo-name'>
+          <div id='repo-name' className='fields-input'>
+            <label className='fields-label'>Text</label>
             <input
               className='text-input'
               placeholder='name'
               name='text'
               type='text'
               value={text}
-              onChange={this.onChange}
+              onChange={this.onHandleChange}
             />
           </div>
-          <div id='repo-stars'>
+          <div id='repo-stars' className='fields-input'>
+            <label className='fields-label'>Stars</label>
             <input
               className='text-input'
               placeholder='stars'
               name='stars'
               type='text'
               value={stars}
-              onChange={this.onChange}
+              onChange={this.onHandleChange}
             />
           </div>
-          <div id='repo-license'>
-            <select name='license' onChange={this.onChange}>
+          <div id='repo-license' className='fields-input'>
+            <label className='fields-label'>License</label>
+            <select name='license' onChange={this.onHandleChange}>
               <License />
             </select>
           </div>
           <div id='repo-forked'>
             <label htmlFor='forked'>
               <input
-                onChange={this.onChange}
+                onChange={this.onHandleChange}
                 type='checkbox'
                 id='forked'
                 name='forked'
@@ -68,7 +71,13 @@ export default class Search extends Component {
               Include Forked
             </label>
           </div>
+          <div id='repo-button'>
+            <button type='submit' id='button'>
+              Search
+            </button>
+          </div>
         </form>
+        <div id='line' />
       </div>
     )
   }
