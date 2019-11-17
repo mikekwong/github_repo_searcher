add24343d5a325303109fdfb3251f8ad83ef0058
Mike Wong <wongmdesign@gmail.com>
2019-11-16 20:01:14 -0500

    fixed onchange for inputs

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,60 +1,63 @@
-import React, { Component } from "react";
-import License from "./License";
-import "./Search.css";
+import React, { Component } from 'react'
+import License from './License'
+import './Search.css'
 
 export default class Search extends Component {
   state = {
-    text: "",
-    stars: "",
-    license: ""
-  };
+    text: '',
+    stars: '',
+    license: ''
+  }
 
   onFormSubmit = e => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+  }
 
-  onInputChange = e => {
+  onChange = e => {
     this.setState({
       [e.target.name]: e.target.value
-    });
-  };
+    })
+    console.log(e.target.value)
+  }
 
-  render() {
+  render () {
     return (
-      <div id="fields">
+      <div id='fields'>
         <form onSubmit={this.onFormSubmit}>
-          <div id="repo-name">
+          <div id='repo-name'>
             <input
-              className="text-input"
-              placeholder="name"
-              type="text"
+              className='text-input'
+              placeholder='name'
+              name='text'
+              type='text'
               value={this.state.text}
-              onChange={this.onInputChange}
+              onChange={this.onChange}
             />
           </div>
-          <div id="repo-stars">
+          <div id='repo-stars'>
             <input
-              className="text-input"
-              placeholder="stars"
-              type="text"
+              className='text-input'
+              placeholder='stars'
+              name='stars'
+              type='text'
               value={this.state.stars}
-              onChange={this.onInputChange}
+              onChange={this.onChange}
             />
           </div>
-          <div id="repo-license">
-            <select name="license">
+          <div id='repo-license'>
+            <select name='license' onChange={this.onChange}>
               <License />
             </select>
           </div>
-          <div id="repo-forked">
-            <label htmlFor="forked">
-              <input type="checkbox" id="forked" name="forked" />
-              <span id="repo-forked-checkmark" />
+          <div id='repo-forked'>
+            <label htmlFor='forked'>
+              <input type='checkbox' id='forked' name='forked' />
+              <span id='repo-forked-checkmark' />
               Include Forked
             </label>
           </div>
         </form>
       </div>
-    );
+    )
   }
 }
