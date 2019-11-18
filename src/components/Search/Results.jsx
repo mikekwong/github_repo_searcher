6f42c5fe18c161968810536c94fd90c08c36ab83
Mike Wong <wongmdesign@gmail.com>
2019-11-18 10:15:19 -0500

    sliced long desc

--- src/components/Search/Results.jsx
+++ src/components/Search/Results.jsx
@@ -3,15 +3,29 @@ import './Results.css'
 
 const Results = ({ result }) => {
   const { full_name, forks, description, stargazers_count, license } = result
+  const descriptionPartOne = firstHalfDescription(description)
+  const descriptionPartTwo = description.slice(descriptionPartOne.length + 1)
   return (
     <div className='results-list'>
       <p>{full_name}</p>
       {/* {forks && <p style={{ backgroundColor: 'gray' }}>forked</p>} */}
-      <p>{description}</p>
+      {description.length > 46 ? (
+        <>
+          <p>{descriptionPartOne}</p>
+          <p>{descriptionPartTwo}</p>
+        </>
+      ) : (
+        <p>{description}</p>
+      )}
       <p>Stars: {stargazers_count}</p>
       {license && <p>{license.name}</p>}
     </div>
   )
+}
+
+function firstHalfDescription (string) {
+  let lastSpaceIndex = string.slice(0, 46).lastIndexOf(' ')
+  return string.slice(0, lastSpaceIndex)
 }
 
 export default Results
